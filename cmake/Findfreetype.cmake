9fc803fd879b4e23b5d242e2f7c6ecc4e12aa67c
wusj7856 <wusj@realbull.net>
2019-11-09 12:04:00 +0800

    add test free type2

---
+++ cmake/Findfreetype.cmake
@@ -0,0 +1,38 @@
+message( "finding FREE_TYPE!"  )
+
+if(WIN32)
+
+    message("Is Windows")
+    set(FREE_TYPE_PATH $ENV{FREE_TYPE_PATH})
+    if( FREE_TYPE_PATH )
+
+        message("Find FREE_TYPE_PATH env!")
+        message(${FREE_TYPE_PATH})
+
+        find_path( FREE_TYPE_INCLUDE_DIR NAMES freetype ft2build.h PATHS "${FREE_TYPE_PATH}/include" )
+        find_library( FREE_TYPE_LIBRARY freetype.lib "${FREE_TYPE_PATH}/objs/x64/Release")
+
+        if( FREE_TYPE_INCLUDE_DIR AND FREE_TYPE_LIBRARY)
+
+            set( FREE_TYPE_FOUND TRUE )
+
+        else()
+
+            set( FREE_TYPE_FOUND FALSE )
+
+        endif()
+
+    else()
+
+        set( FREE_TYPE_FOUND FALSE )
+        message("Not Find FREE_TYPE_PATH env!")
+
+    endif()
+
+else()
+
+    message("Not Windows!")
+
+endif()
+
+message("................................................................")
